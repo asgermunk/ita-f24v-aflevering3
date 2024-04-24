6abc236f0b0d1e5b0e17529a27ba6e8a87658704
asgermunk <75023079+asgermunk@users.noreply.github.com>
2024-04-24 19:45:17 +0200

    Add files via upload

---
+++ ita-f24v-aflevering3-main/nytscript.js
@@ -0,0 +1,37 @@
+// Load the JSON data from the 'albums.json' file
+d3.json("albums.json").then((data) => {
+    // Create a Set from the genres in the data to get unique genres
+    const genreSet = new Set(data.map((item) => item.genre));
+    // Convert the Set back to an Array
+    const genres = Array.from(genreSet);
+  
+    // Select the dropdown list with id 'genre-select'
+    const select = d3.select("#genre-select");
+    // Bind the genres data to the 'option' elements of the dropdown list
+    select
+      .selectAll("option")
+      .data(genres)
+      .enter() // Create new 'option' elements for each item in the data that doesn't already have an associated 'option' element
+      .append("option") // Append the new 'option' elements to the dropdown list
+      .text((d) => d); // Set the text of each 'option' element to be the genre
+  
+    // Set the text of each 'div' element to be the album name and artist name
+    select.on("change", function () {
+      const selectedGenre = d3.select(this).property("value");
+      const filteredAlbums = data.filter((item) => item.genre === selectedGenre);
+      const list = d3.select("#albums-list");
+      list.selectAll("tr").remove();
+  
+      const rows = list
+        .selectAll("tr")
+        .data(filteredAlbums)
+        .enter()
+        .append("tr");
+  
+      rows.append("td").text((d) => d.albumName);
+  
+      rows.append("td").text((d) => d.artistName);
+  
+      rows.append("td").text((d) => d.productionYear);
+    });
+  });
